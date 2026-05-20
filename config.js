87e0120c57e00aba793ea06bcf0525c636103645
counciltu-source <council.tu@tu.ac.th>
2026-05-20 14:49:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the deployed Google Apps Script Web App URL here before publishing to GitHub Pages.
 // Example: window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxGsfYaxZqcbA_hvZ0Yb7x2J5GDhuLhbs_3GbBrHLMaF2K3ibq1mxPmY3Xh5bdKJtV1/exec';
-window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxGsfYaxZqcbA_hvZ0Yb7x2J5GDhuLhbs_3GbBrHLMaF2K3ibq1mxPmY3Xh5bdKJtV1/exec';
+window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxGsfYaxZqcbA_hvZ0Yb7x2J5GDhuLhbs_3GbBrHLMaF2K3ibq1mxPmY3Xh5bdKJtV1/exec?authuser=0';
